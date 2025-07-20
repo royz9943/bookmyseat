6d125ef84008e1b26702f8e8b3df38788b672584
saurav-GLA <saurav.rai@gla.ac.in>
2025-07-20 23:09:36 +0530

    debug true

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v9erai*+cv_%p$l-p#cysgm!rt8m=@+-y&=kgmu1!-!czdzece'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-14414.up.railway.app']
 
